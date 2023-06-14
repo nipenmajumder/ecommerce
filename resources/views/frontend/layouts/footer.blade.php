6b4cbb4337ddf75c7210e28455d15c65e5041acc
Nipen <amitmojumder356@gmail.com>
2023-06-14 07:40:59 +0600

    frontend mastaring

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,57 +1,50 @@
-<style>
-    .footer-links a {
-        text-decoration: none; /* Remove underline */
-        color: #888; /* Set default text color to gray */
-    }
-
-    .footer-links a:hover {
-        color: white; /* Set text color to white on hover */
-    }
-</style>
-
 <footer class="bg-dark p-3 mt-5">
     <div class="container">
         <div class="row">
             <div class="col-lg-3 p-2 text-light">
                 <img src="{{asset('frontend/image/banner/wafi-bangla-logo.webp')}}" alt="">
-                <p>
-                    Wafilife is a leading book shop in Bangladesh. We offer thousands of Islamic, general, and academic books at a discounted price. We provide good packaging with low shipping cost all over Bangladesh.
-                </p>
+                <p>Wafilife is a leading book shop in Bangladesh. We offer thousands of islamic, general and
+                    academic books at a discounted price. We provide good packaging with low shipping cost all over
+                    the Bangladesh.</p>
                 <h4>অ্যাপ ডাউনলোড করুন</h4>
                 <img src="{{asset('frontend/image/banner/en_badge_web_generic.png')}}" class="img-fluid w-50" alt="">
+
             </div>
             <div class="col-lg-3 p-2 text-light">
-                <h4>প্রয়োজনীয় লিংক</h4>
-                <ul class="list-unstyled footer-links">
-                    <li><a href="#">যোগাযোগ করুন</a></li>
-                    <li><a href="#">ব্লগ</a></li>
-                    <li><a href="#">শপিং ব্যাগ</a></li>
-                    <li><a href="#">প্রশ্নোত্তর</a></li>
-                    <li><a href="#">কিভাবে কেনাকাটা করবেন?</a></li>
-                    <li><a href="#">শর্তাবলী</a></li>
-                    <li><a href="#">রিফান্ড নীতিমালা</a></li>
-                    <li><a href="#">প্রাইভেসী পলিসি</a></li>
-                </ul>
+                <h5>প্রয়োজনীয় লিংক</h5>
+
+                <nav class="nav flex-column">
+                    <a class="nav-link text-white" href="#">যোগাযোগ করুন</a>
+                    <a class="nav-link text-white" href="#">ব্লগ</a>
+                    <a class="nav-link text-white" href="#">শপিং ব্যাগ</a>
+                    <a class="nav-link text-white" href="#">প্রশ্নোত্তর</a>
+                    <a class="nav-link text-white" href="#">কিভাবে কেনাকাটা করবেন ?</a>
+                    <a class="nav-link text-white" href="#">শর্তাবলী</a>
+                    <a class="nav-link text-white" href="#">রিফান্ড নীতিমালা</a>
+                    <a class="nav-link text-white" href="#">প্রাইভেসী পলিসি</a>
+                </nav>
             </div>
             <div class="col-lg-3 p-2 text-light">
-                <h4>জনপ্রিয়</h4>
-                <ul class="list-unstyled footer-links">
-                    <li><a href="#">আপনার পছন্দের তালিকা</a></li>
-                    <li><a href="#">জেনারেল ও অ্যাকাডেমিক বই</a></li>
-                    <li><a href="#">ড. খোন্দকার আব্দুল্লাহ জাহাঙ্গীর এর বই</a></li>
-                    <li><a href="#">আরিফ আজাদ এর বই</a></li>
-                    <li><a href="#">প্রি-অর্ডার</a></li>
-                    <li><a href="#">প্যাকেজ</a></li>
-                    <li><a href="#">ইলেকট্রনিক্স</a></li>
-                </ul>
+                <h5>জনপ্রিয়</h5>
+                <nav class="nav flex-column">
+                    <a class="nav-link text-white" href="#">আপনার পছন্দের তালিকা</a>
+                    <a class="nav-link text-white" href="#">জেনারেল ও অ্যাকাডেমিক বই</a>
+                    <a class="nav-link text-white" href="#">ড. খোন্দকার আব্দুল্লাহ জাহাঙ্গীর এর বই</a>
+                    <a class="nav-link text-white" href="#">আরিফ আজাদ এর বই</a>
+                    <a class="nav-link text-white" href="#">প্রি-অর্ডার</a>
+                    <a class="nav-link text-white" href="#">প্যাকেজ</a>
+                    <a class="nav-link text-white" href="#">ইলেকট্রনিক্স</a>
+                </nav>
             </div>
             <div class="col-lg-3 p-2 text-light">
-                <h4>যোগাযোগ</h4>
-                <p>
-                    Head Office: House 310, Road 21, Mohakhali DOHS, Dhaka-1206<br>
-                    Phone: 017-9992-5050, 096-7877-1365<br>
-                    Email: sales@wafilife.com
-                </p>
+                <h5>যোগাযোগ</h5>
+                <nav class="nav flex-column">
+                    <a class="nav-link text-white" href="#">Head Office: House 310, Road 21
+                        Mohakhali DOHS, Dhaka-1206</a>
+                    <a class="nav-link text-white" href="#">Phone:
+                        017-9992-5050 096-7877-1365</a>
+                    <a class="nav-link text-white" href="#">sales@wafilife.com</a>
+                </nav>
             </div>
         </div>
     </div>
